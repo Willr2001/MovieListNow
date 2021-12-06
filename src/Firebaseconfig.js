fe1bdda1a349262a58a8ccc28913b2a88216bd18
Will Ruskin <willjruskin@gmail.com>
2021-12-06 11:25:24 -0800

    adding auth

---
+++ src/Firebaseconfig.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCW5D177W4rKoptgOBxDvQ7M4tpyGnu1sA",
+  authDomain: "movie-suggester-3e1c3.firebaseapp.com",
+  projectId: "movie-suggester-3e1c3",
+  storageBucket: "movie-suggester-3e1c3.appspot.com",
+  messagingSenderId: "78103394431",
+  appId: "1:78103394431:web:cd8bad1e714a284be9274a"
+};
